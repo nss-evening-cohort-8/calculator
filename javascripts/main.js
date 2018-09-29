fe3a1d20a16be89b256a2ca5bb9ddd7d7de01b10
Zoe Ames <zoeames@gmail.com>
2018-09-29 12:33:48 -0500

    added 7 event listener

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,12 @@
 import {calculate} from './components/calculator.js';
+import buttonEvents from './helpers/buttonEvents.js';
 
-// calculate(4, 2, 'multiply'); //8
+const initializeApp = () => {
+  buttonEvents();
+  // calculate(4, 2, 'multiply'); //8
+  // calculate(4, 2, 'divide'); //2
+  // calculate(14, 2, 'add'); //16
+  calculate(44, 2, 'subtract'); //42
+};
 
-// calculate(4, 2, 'divide'); //2
-// calculate(14, 2, 'add'); //16
-calculate(44, 2, 'subtract'); //42
+initializeApp();
