5905be74a19274baee2c81b4101f323561f39844
Zoe Ames <zoeames@gmail.com>
2018-09-29 13:15:00 -0500

    saving to first number working, initial display working

--- javascripts/components/calculator.js
+++ javascripts/components/calculator.js
@@ -1,6 +1,13 @@
 import {printToDom} from '../helpers/util.js';
 import {add, divide, multiply, subtract} from '../helpers/maths.js';
 
+const calc = {
+  firstNumber: '',
+  secondNumber: '',
+  mathType: '',
+  display: ''
+};
+
 const calculate = (num1, num2, mathType) => {
   let answer = 0;
   switch(mathType){
@@ -19,7 +26,26 @@ const calculate = (num1, num2, mathType) => {
     default:
       answer = 'nope';
   }
-  printToDom(answer, 'result');
+  setDisplay(answer);
 };
 
-export {calculate};
+const setDisplay = (someNumber) => {
+  calc.display = someNumber;
+  printToDom(calc.display, 'result');
+};
+
+const initialDisplay = () => {
+  printToDom(0, 'result');
+};
+
+const addNumber = (num) => {
+  if(calc.mathType === '') {
+    calc.firstNumber += num;
+    setDisplay(calc.firstNumber);
+  } else {
+    calc.secondNumber += num;
+    setDisplay(calc.secondNumber);
+  }
+};
+
+export {calculate, addNumber, initialDisplay};
